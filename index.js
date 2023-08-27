1095c68cf9e18b19ea5f28e8bac6c9f7422ac6ec
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-08-27 13:45:33 -0400

    completed index.js file as specified

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove("main");
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent =  "Yisel is the champion";
+document.body.append(newHeader);
